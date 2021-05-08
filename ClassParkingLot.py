f8387235d39d47c6b77bfc9d2ba58f6b35f7bb29
PavlovVladislav <36258479+PavlovVladislav@users.noreply.github.com>
2021-05-08 14:31:32 +0300

    Feat:
    Add adding vehicles to the parking lot
    
    Parking scheme is now displayed through counting free and occupied spaces, not through an array

--- ClassParkingLot.py
+++ ClassParkingLot.py
@@ -1,37 +1,62 @@
 class ParkingLot:
     typePlace = ["Moto place", "Smart place", "Large place"]
 
-    transport = {
-        "Moto": 0,
-        "Auto": 0,
-        "Bus": 0
-    }
-
     def __init__(self, name, motorcycle_place, smart_place, large_place):
         self.name = name
-        self.motorcycle_place = motorcycle_place
-        self.smart_place = smart_place
-        self.large_place = large_place
-        self.schema = [
-            [0] * self.motorcycle_place,
-            [0] * self.smart_place,
-            [0] * self.large_place
-        ]
+        self.arr_place = [motorcycle_place, smart_place, large_place]
+        self.typePlace = {
+            "Moto place": self.arr_place[0],
+            "Smart place": self.arr_place[1],
+            "Large place": self.arr_place[2]
+        }
+        self.transport = {
+            "Moto": 0,
+            "Auto": 0,
+            "Bus": 0
+        }
 
     def show_free_place(self):
-        for i in range(len(self.schema)):
-            print(str(self.typePlace[i]) + " has " + str(self.schema[i].count(0)) + " free.")
+        for key in self.typePlace:
+            print(str(key) + " has " + str(self.typePlace[key]) + " free.")
         print()
 
-    def show_schema(self):
-        for i in range(len(self.schema)):
-            print(self.typePlace[i], end=':')
-            for j in range(len(self.schema[i])):
-                print(self.schema[i][j], end=' ')
-            print()
+    def draw_schema(self):
+        i = 0
+        for key in self.typePlace:
+            print(str(key) + " : " + "1 " * (self.arr_place[i] - self.typePlace[key]) + "0 " * self.typePlace[key])
+            i += 1
         print()
 
     def show_info(self):
         print("Moto on parking lot: " + str(self.transport["Moto"]))
         print("Auto on parking lot: " + str(self.transport["Auto"]))
-        print("Bus on parking lot: " + str(self.transport["Bus"]))
+        print("Bus on parking lot: " + str(self.transport["Bus"]), end='\n\n')
+
+    def add_transport(self, type_transport):
+        if type_transport == "Moto":
+            for key in self.typePlace:
+                if self.typePlace[key] != 0:
+                    self.typePlace[key] = self.typePlace[key] - 1
+                    self.transport["Moto"] += 1
+                    print("Added 1 Moto", end='\n\n')
+                    break
+                else:
+                    print("Don/'t have place for Moto", end='\n\n')
+
+        elif type_transport == "Auto":
+            if self.typePlace["Smart place"] != 0:
+                self.typePlace["Smart place"] = self.typePlace["Smart place"] - 1
+                self.transport["Auto"] += 1
+            elif self.typePlace["Large place"] != 0:
+                self.typePlace["Large place"] = self.typePlace["Large place"] - 1
+                self.transport["Auto"] += 1
+            else:
+                print("Don/'t have place for Auto", end='\n\n')
+
+        elif type_transport == "Bus":
+            if self.typePlace["Large place"] // 5 != 0:
+                self.typePlace["Large place"] = self.typePlace["Large place"] - 5
+                self.transport["Bus"] += 1
+            else:
+                print("Don\'t have place for Bus", end='\n\n')
+
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from ClassParkingLot import ParkingLot
 
-parkingLot = ParkingLot('Moscow', 3, 5, 15)
-parkingLot.show_schema()
-parkingLot.show_free_place()
-parkingLot.show_info()
+if __name__ == '__main__':
+    parkingLot = ParkingLot('Moscow', 3, 5, 15)
+    parkingLot.draw_schema()
+    parkingLot.show_free_place()
+    parkingLot.show_info()
+    parkingLot.add_transport("Moto")
+    parkingLot.draw_schema()
+    for i in range(4):
+        parkingLot.add_transport("Bus")
+    parkingLot.draw_schema()
