c1a135b071540bbf53d33a367d079c816175e1aa
Bragi <beluerosy@gmail.com>
2021-05-08 17:53:23 +0300

    Feat:
    Add remote transport from parking lot

--- ClassParkingLot.py
+++ ClassParkingLot.py
@@ -60,9 +60,46 @@ class ParkingLot:
         elif type_transport == "Bus":
             if self.schema[2].count(0) // 5 != 0:
                 start = self.schema[2].index(0)
-                for i in range(4):
-                    self.schema[2][i] = "B"
+                for i in range(5):
+                    self.schema[2][start + i] = "B"
                 self.transport["Bus"] += 1
                 print("Added 1 Bus", end='\n\n')
             else:
                 print("Don\'t have place for Bus", end='\n\n')
+
+    def del_transport(self, type_transport):
+        if type_transport == "Moto":
+            for i in range(len(self.typePlace)):
+                if self.schema[i].count('M') != 0:
+                    self.schema[i].reverse()
+                    self.schema[i][self.schema[i].index('M')] = 0
+                    self.schema[i].reverse()
+                    self.transport["Moto"] -= 1
+                    print("Removed 1 Moto", end='\n\n')
+                    break
+                else:
+                    print("All place for Moto are free", end='\n\n')
+
+        elif type_transport == "Auto":
+            for i in range(1, len(self.typePlace)):
+                if self.schema[i].count('A') != 0:
+                    self.schema[i].reverse()
+                    self.schema[i][self.schema[i].index('A')] = 0
+                    self.schema[i].reverse()
+                    self.transport["Auto"] -= 1
+                    print("Removed 1 Auto", end='\n\n')
+                    break
+                else:
+                    print("All place for Auto are free", end='\n\n')
+
+        elif type_transport == "Bus":
+            if self.schema[2].count(0) // 5 != 0:
+                self.schema[2].reverse()
+                start = self.schema[2].index('B')
+                for i in range(5):
+                    self.schema[2][start + i] = 0
+                self.schema[2].reverse()
+                self.transport["Bus"] += 1
+                print("Removed 1 Bus", end='\n\n')
+            else:
+                print("All place for Bus are free", end='\n\n')
